aaea7687ce620d58c72d8e09215a79b86d4d4ab5
Ilya-bunat <ilya.binat15@gmail.com>
2025-11-09 12:37:35 +0300

    regester model

--- journal/users/models.py
+++ journal/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class CustomUser (AbstractUser):
+    STUDENT = 'student'
+    COACH = 'coach'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = [
+        (STUDENT, 'Спортсмен'),
+        (COACH, 'Тренер'),
+        (ADMIN, 'Admin')
+    ]
+    role = models.CharField(max_length=255,choices=ROLE_CHOICES)
+    middle_name = models.CharField(max_length=255, null=True, blank=True)# null=True, blank=True - Делают поля не обязательными для заполнения
+    birth_date = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
+    
 # Create your models here.
