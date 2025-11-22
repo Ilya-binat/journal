076dd57c2e502995af07996df774c5fb427bae38
Ilya-bunat <ilya.binat15@gmail.com>
2025-11-22 22:58:30 +0300

    изменили функцию register чтобы email c формы сохранялся в username

--- journal/users/views.py
+++ journal/users/views.py
@@ -4,15 +4,19 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 
+
 def register(request):
     form = RegisterForm(request.POST or None)
 
-    if form.is_valid():
-        form.save()
-        return redirect(request, 'log_in.html', {'form:form'})
-    
+    if request.method == "POST" and form.is_valid():
+        user = form.save(commit=False)  # не сохраняем сразу
+        user.username = form.cleaned_data["email"]  # подставляем email
+        user.save()  # теперь сохраняем
+        return redirect("users:log_in")  # переход на логин-страницу
 
-    return render(request, 'register.html', {'form':form})
+    return render(request, "register.html", {"form": form})
+
+
 
 def log_in(request):
 
@@ -20,23 +24,27 @@ def log_in(request):
 
     if form.is_valid():
         user = form.get_user()
-        login(request,user)
+        login(request, user)
 
-        return redirect('users:profile')
-    
-    return render(request, 'log_in.html', {'form':form})
+        return redirect("users:profile")
+
+    return render(request, "log_in.html", {"form": form})
+
 
 def log_out(request):
     logout(request)
-    return redirect('users:log_in')    
+    return redirect("users:log_in")
+
 
 def home(request):
-    form = LoginForm()  
-    return render(request, 'home.html', {'form': form}) 
+    form = LoginForm()
+    return render(request, "home.html", {"form": form})
 
-@login_required# нужно импортировать login_required
+
+@login_required  # нужно импортировать login_required
 def profile(request):
-    return render(request, 'profile.html', {'user_obj':request.user})
+    return render(request, "profile.html", {"user_obj": request.user})
+
 
 @login_required
 def update_avatar(request):
@@ -45,21 +53,24 @@ def update_avatar(request):
         if avatar:
             request.user.profile.image = avatar
             request.user.profile.save()
-        return redirect('users:profile')
+        return redirect("users:profile")
 
-    return redirect('users:profile')
+    return redirect("users:profile")
+
 
 @login_required
 def edit_profile(request):
-    return render(request, 'users/edit_profile.html')
+    return render(request, "users/edit_profile.html")
+
 
 @login_required
 def change_password(request):
-    return render(request, 'users/change_password.html')
-
+    return render(request, "users/change_password.html")
 
 
 @login_required
 def profile_update(request):
-    return render(request, 'users/profile_update.html')
+    return render(request, "users/profile_update.html")
+
+
 # Create your views here.
